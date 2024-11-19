afcf5315467a455d0b85a7781abfa731edbb8282
Yehia El Marghany <yehiaseif2005@gmail.com>
2024-11-19 18:22:30 +0200

    hi

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const mongoURI =
 
 mongoose
   .connect(mongoURI)
-  .then(() => console.log("Connected to MongoDB"))
+  .then(() => console.log("Connected to MongoDB1"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
 // Basic route
