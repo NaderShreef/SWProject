a3c4e70de1dcb18d6897e083849d095dc249714a
Jana <janaAli12>
2024-11-20 11:02:57 -0800

    j

---
+++ backend/src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { AppService } from './app.service';
+import { config } from 'dotenv'; 
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI, {
+      dbName: process.env.DATABASE_NAME,
+    }),
+  ],
+  providers: [AppService],
+})
+export class AppModule {}
