533de1f90229185dc42deb90dc1174c9cf83fea8
NaderShreef <nadersgn@gmail.com>
2024-11-20 22:18:47 +0200

    Nader app fix 2

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -2,7 +2,18 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  private mongoUri = 'mongodb://localhost:27017';
+  private databaseName = 'myDatabase';
+
   getHello(): string {
     return 'Hello, MongoDB is connected!';
+  }
+
+  getMongoUri(): string {
+    return this.mongoUri;
+  }
+
+  getDatabaseName(): string {
+    return this.databaseName;
   }
 }
