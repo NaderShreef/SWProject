162174c2bde2aa72124252ba7729bc5a58d19bcd
NaderShreef <nadersgn@gmail.com>
2024-11-23 23:52:34 +0200

    Nader fixes

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,7 +5,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProgressModule } from './progress/progress.Module';
 import { ProgressSchema } from './progress/progress.schema';
-=======
+import { ProgressService } from './progress/progress.service';
+import { ProgressController } from './progress/progress.controller';
 
 @Module({
   imports: [
@@ -16,7 +17,6 @@ import { ProgressSchema } from './progress/progress.schema';
       dbName: process.env.DATABASE_NAME,
     }),
     ProgressModule,
-=======
   ],
   controllers: [AppController],
   providers: [AppService],
