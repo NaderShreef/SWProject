6015ee2db3fc729ba9c9cde640a5cdc432836070
Nagham <NaghamShady>
2024-11-23 12:50:51 +0200

    test

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
-import { NotesModule } from './notes/notes.module';
+import { NotesModule } from './notes/notes.Schema';
 
 @Module({
   imports: [
