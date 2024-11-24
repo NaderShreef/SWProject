e6a42a8df4fc6aff0d5c750a5810733c2fdbd612
NaderShreef <nadersgn@gmail.com>
2024-11-24 21:11:25 +0200

    Nader imports

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,12 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { QuizModule } from './quizzes/quiz.module';
+import { ResponsesModule } from './responses/responses.module';
+import { UsersModule } from './users/user.module';
+import { ProgressModule } from './progress/progress.module';
 import { NotesModule } from './notes/notes.Schema';
-=======
-import { ProgressModule } from './progress/progress.Module';
-import { ProgressSchema } from './progress/progress.schema';
-import { ProgressService } from './progress/progress.service';
-import { ProgressController } from './progress/progress.controller';
+import { ModulesModule } from './modules/modules.module';
+import { InteractionsModule } from './Data_science/interactions.module';
+import { RecommendationsModule } from './Data_science/recommendations.module';
 
 @Module({
   imports: [
@@ -18,6 +20,14 @@ import { ProgressController } from './progress/progress.controller';
     MongooseModule.forRoot(process.env.MONGO_URI, {
       dbName: process.env.DATABASE_NAME,
     }),
+    QuizModule,
+    ResponsesModule,
+    UsersModule,
+    ProgressModule,
+    NotesModule,
+    ModulesModule,
+    InteractionsModule,
+    RecommendationsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- backend/src/progress/progress.module.ts
+++ backend/src/progress/progress.module.ts
@@ -1,13 +1,14 @@
-import { Module } from "@nestjs/common";
-import { ProgressController } from "./progress.controller";
-import { ProgressService } from "./progress.service";
-import { MongooseModule } from "@nestjs/mongoose";
-import { ProgressSchema } from "./progress.schema";
+import { Module } from '@nestjs/common';
+import { ProgressService } from './progress.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProgressSchema } from './progress.schema';
+import { ProgressController } from './progress.controller';
+
 @Module({
-    imports:[ MongooseModule.forFeature([{name : 'progress', schema: ProgressSchema}])],
-    controllers : [ProgressController],
-    providers : [ProgressService]
+  imports: [
+    MongooseModule.forFeature([{ name: 'progress', schema: ProgressSchema }]),
+  ],
+  controllers: [ProgressController],
+  providers: [ProgressService],
 })
-export class ProgressModule{
-    
-}
+export class ProgressModule {}
