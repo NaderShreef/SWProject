20aad3ef5f68c0ba77edf2a12edc0d820e1afe90
Jabary22 <abdelrahmanjabry@gmail.com>
2024-12-21 14:42:20 +0200

    dd

--- frontend/app/app/about/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function about(){
-    return (<h1>hi from about</h1>)
-}
--- frontend/app/app/utils/axiosInstance.ts
+++
@@ -1,9 +0,0 @@
-// utils/axiosInstance.ts
-import axios from 'axios';
-
-const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL , // Use environment variable for base URL
-  withCredentials: true, // Include cookies if needed
-});
-
-export default axiosInstance;
