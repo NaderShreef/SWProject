38e79dd406fe9dcd5755b6f7f94ef5da1633397b
mahmouddfouda <mahmoud.foda@student.giu-uni.de>
2024-12-24 17:55:55 +0200

    frontend ddee

--- backend/dist/main.js
+++ backend/dist/main.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const app_service_1 = require("./app.service");
+const cookieParser = require("cookie-parser");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     const appService = app.get(app_service_1.AppService);
@@ -14,6 +15,7 @@ async function bootstrap() {
         allowedHeaders: 'Content-Type, Accept, Authorization',
         credentials: true
     });
+    app.use(cookieParser());
     const port = 5001;
     await app.listen(port);
     console.log(`Application is running on http://localhost:${port}`);
